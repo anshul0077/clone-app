21f68df9e7d645614170f65b9427cf02607822cc
Anshul <choudharyanshul@gmail.com>
2025-03-13 22:59:20 +0530

    playing with js

--- src/script.js
+++ src/script.js
@@ -2,19 +2,21 @@ let more=document.querySelector("#left-more")
 let account=document.querySelector("#account")
 let options=document.querySelector(".more-options")
 let AccountOptions=document.querySelector(".more-account")
+let songDiv= document.querySelector(".main-content")
 let music=[
-    {name:"Rehan Deyan", src:"./songs/Rehan-Deyan.mp3",img:"./songs/Rehan deyan.jpg"},
-    {name:"Majhail Anthem", src:"./songs/Majhail Anthem.mp3",img:"./songs/MAjhail.jpg"},
-    {name:"Jhol", src:"./songs/Jhol.mp3",img:"./songs/Jhol.jpg"},
-    {name:"Rukh", src:"./songs/Rukh.mp3",img:"./songs/Rukh.jpg"},
+    {id:0, name:"Rehan Deyan", src:"./songs/Rehan-Deyan.mp3",img:"./songs/Rehan-deyan.jpg"},
+    {id:1, name:"Majhail Anthem", src:"./songs/Majhail Anthem.mp3",img:"./songs/MAjhail.jpg"},
+    {id:2, name:"Jhol", src:"./songs/Jhol.mp3",img:"./songs/Jhol.jpg"},
+    {id:3, name:"Rukh", src:"./songs/Rukh.mp3",img:"./songs/Rukh.jpg"},
 
 ]
+let clutter=""
 music.forEach(e=>{
-    console.log(e.img);
-    
+clutter+=`<div class="box h-12 rounded-md bg-gray-700 m-3 w-[90%] flex items-center justify-start gap-4"> <div class="left  flex items-center justify-start w-[78%] gap-4"><div class="img h-full rounded-md  w-14"> <img  class="rounded-md h-full w-[95%] " src=${e.img} alt=""></div> <p>${e.name}</p> </div>   <i data-id=${e.id} class="play-btn ri-play-mini-fill  p-1 px-2 text-2xl  flex items-center justify-center text-black bg-green-500 rounded-full  "></i>  </div>
+`
+
+songDiv.innerHTML=clutter
 })
-let mm =new Audio("./songs/Rehan-Deyan.mp3")
-mm.play()
 let flag=0
 let flag2=0
 more.addEventListener("click",()=>{
@@ -38,4 +40,20 @@ flag2=1
 flag2=0
     }
     
+})
+
+songDiv.addEventListener('click',(e)=>{
+    let    audio = new Audio ("")
+    console.log(audio);
+    
+    if(e.target.tagName==="I"){
+        let id=parseInt((e.target.getAttribute("data-id")))
+        let musicSrc=(music[id].src)
+        
+     audio = new Audio (musicSrc)
+    
+audio.play()
+console.log(audio);
+
+}
 })
