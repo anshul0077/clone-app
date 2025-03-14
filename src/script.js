58f66cde6b1142e6add7409a6af5df14b17063ac
Anshul <choudharyanshul@gmail.com>
2025-03-14 22:33:13 +0530

    addin play and pause fxn

--- src/script.js
+++ src/script.js
@@ -4,10 +4,10 @@ let options=document.querySelector(".more-options")
 let AccountOptions=document.querySelector(".more-account")
 let songDiv= document.querySelector(".main-content")
 let music=[
-    {id:0, name:"Rehan Deyan", src:"./songs/Rehan-Deyan.mp3",img:"./songs/Rehan-deyan.jpg"},
-    {id:1, name:"Majhail Anthem", src:"./songs/Majhail Anthem.mp3",img:"./songs/MAjhail.jpg"},
-    {id:2, name:"Jhol", src:"./songs/Jhol.mp3",img:"./songs/Jhol.jpg"},
-    {id:3, name:"Rukh", src:"./songs/Rukh.mp3",img:"./songs/Rukh.jpg"},
+    {id:0, name:"Rehan Deyan", src:"./songs/Rehan-Deyan.mp3",img:"./songs/Rehan-deyan.jpg",artist:'Navaan sandhu'},
+    {id:1, name:"Majhail Anthem", src:"./songs/Majhail.mp3",img:"./songs/Majh.jpg" ,artist:'Karan Randhawa'},
+    {id:2, name:"Jhol", src:"./songs/Jhol.mp3",img:"./songs/Jhol.jpg",artist:'Mannu, Annural Khalid' },
+    {id:3, name:"Rukh", src:"./songs/Rukh.mp3",img:"./songs/Rukh.jpg " ,artist:'Navaan sandhu'},
 
 ]
 let clutter=""
@@ -19,6 +19,7 @@ songDiv.innerHTML=clutter
 })
 let flag=0
 let flag2=0
+
 more.addEventListener("click",()=>{
     if(flag==0){
         options.style.display="block"
@@ -42,18 +43,69 @@ flag2=0
     
 })
 
-songDiv.addEventListener('click',(e)=>{
-    let    audio = new Audio ("")
-    console.log(audio);
-    
-    if(e.target.tagName==="I"){
-        let id=parseInt((e.target.getAttribute("data-id")))
-        let musicSrc=(music[id].src)
-        
-     audio = new Audio (musicSrc)
-    
-audio.play()
-console.log(audio);
+
+let currentAudio = null; // To track the currently playing audio
+
+songDiv.addEventListener('click', (e) => {
+    if (e.target.tagName === "I") {
+        let id = parseInt(e.target.getAttribute("data-id"));
+        let musicSrc = music[id].src;
+
+        // If there's a song currently playing and it's the same as the selected song, toggle play/pause
+        if (currentAudio && currentAudio.src.includes(musicSrc)) {
+            if (currentAudio.paused) {
+                currentAudio.play();
+            } else {
+                currentAudio.pause();
+            }
+        } else {
+            // Stop any currently playing song
+            if (currentAudio) {
+                currentAudio.pause();
+            }
+
+            // Create a new audio instance and play the selected song
+            currentAudio = new Audio(musicSrc);
+            currentAudio.play();
+        }
+
+        playbackSongs(id);
+    }
+});
+
+
+let leftplayback=document.querySelector(".left-playback")
+function playbackSongs(id){
+let showcase=music[id].img
+let artistName=music[id].artist
+let songname=music[id].name
+console.log(showcase);
+
+let clutter=''
+clutter+=`    <div class="img h-24 w-24 bg-red-500 rounded-md">
+               <img class="h-[100%] rounded-md w-[100%]" src=${showcase}>
+                </div>
+                <div class="content">
+                    <p class="font-semibold ">${songname}</p>
+                    <p class="text-gray-300">${artistName}</p>
+                </div>
+            
+                <i class="ri-add-circle-line w-10 h-10  text-xl flex items-center justify-center rounded-full"></i>`
+
+
+                leftplayback.innerHTML=clutter
+
 
 }
+let volumeRocker=document.querySelector("#volume-rocker")
+
+volumeRocker.addEventListener('input',()=>{
+currentAudio.volume=volumeRocker.value
+    
 })
+function seek(){
+let seek=document.querySelector("#seek")
+
+}
+seek()
+
